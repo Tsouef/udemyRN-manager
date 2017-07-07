77ff173f45ea0600e258490b642ff69899e211d6
Thibault Souef <tsouef@letudiant.fr>
2017-07-07 18:19:44 +0200

    wip

---
+++ src/actions/EmployeeActions.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE
+} from './types';
+
+export const employeeUpdate = ({ prop, value }) => {
+  return {
+    type: EMPLOYEE_UPDATE,
+    payload: { prop, value }
+  };
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+  console.log(name, phone, shift);
+};
